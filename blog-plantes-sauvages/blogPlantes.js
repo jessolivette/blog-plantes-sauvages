bbafed69c630ec349b1b09df4a85da15b3cf6203
aldegonde_salsifis <jessolivette@hotmail.fr>
2019-05-13 15:01:38 +0200

    setting html content to js file, adding class (to toggle) in css.

--- blog-plantes-sauvages/blogPlantes.js
+++ blog-plantes-sauvages/blogPlantes.js
@@ -1,78 +1,31 @@
-
-var name = document.getElementById("name").textContent;
-var description = document.getElementById("description").textContent;
-var shape = document.getElementById("shape").textContent;
-
-// le block suivant est reproduit pour chaque plante (dans l'attente d'une solution plus efficace ?)
-// CSS à venir pour la présentation, l'élément textContent permet normalement la récupération des attributs de style.
-
-//event listeners
-var achillee = document.getElementById("achillee");
-achillee.addEventListener("click", ficheAchillee);
-
-var ail = document.getElementById("ail");
-ail.addEventListener("click", ficheAil);
-
-var bourrache = document.getElementById("bourrache");
-bourrache.addEventListener("click", ficheBourrache);
-
-var camomille = document.getElementById("camomille");
-camomille.addEventListener("click", ficheCamomille);
+function render() {
+    // this array stores li elements content.
+    var plants = ["<span>Achillée millefeuille <i>(Achillea millefolium)</i></span><p>Antispasmodique, décongestionnante, peptique pour les troubles digestifs, cicatrisante, coagulante, anti-inflamatoire, antiseptique pour traiter les plaies même ouvertes, très utiles contre les règles très abondantes ou douloureuses.</p><p>usage interne: en tisane ou teinture mère pour la digestion et les cycles menstruels (fleurs, feuilles), en cataplasme sur les plaies (feuilles).</p>", "<span>Ail des ours <i>(Allium ursinum)</i></span><p class='ail'>dépuratif, hypotenseur, antiseptique, rubéfiant (soulage les douleurs dues aux rhumatismes), vermifuge, très riche en vitamine C.</p><p class='ail'>on consomme les feuilles fraiches en salade, peut être utilisé comme plante aromatique ou cuisiné en soupes et sauces (pesto d'ail des ours), on en fait également de liqueur.</p>", "<span>Bourrache <i>(Borago officinalis)</i></span><p class='bourrache'>sudorifique, diurétique, favorise le transit intestinal, lutte contre les dermatoses (herpes, eczéma, psoryasis) et le vieillissement cutané, aphrodisiaque.</p><p class='bourrache'>usage externe : huile, interne : tisane (feuilles, fleurs séchées), fleurs fraiches pour décorer les salades. En interne il est recommandé de limiter la consomation, a fortes doses la bourrache est toxique.</p>", "<span>Camomille matricaire <i>(Matricaria chamomilla)</i></span><p class='camomille'>anti-inflamatoire, anti-bactérien, anti-fongique, sédatif. Traite les troubles digestifs, l'irritation occulaire ou sert d'antiprurigineux sur les crevasses, piqures d'insectes, écorchures.</p><p class='camomille'>Principalement en tisane. L'infusion refroidie peut être appliquée directement dans l'oeil, en cataplasme.</p>", "<span>Chenopode blanc <i>(Chenopodium album)</i></span><p class='chenopode'>alimentation générale et carences : riche en protéines, vitamines A, C, B1, B2, PP et sels minéraux.</p><p class='chenopode'>à consommer comme les épinards pour les feuilles (cuites ou en salades), les graines peuvent être moulues en farine ou cuites comme des céréales, les racines écrasées sont un substitu de savon doux efficace.</p>", "<span>Consoude <i>(Symphytum)</i></span><p class='consoude'>accélère la consolidation des fractures, la cicatrisation des plaies et des ulcères gastriques. <b class='danger'>attention</b>: consommée régulièrement la consoude est dangereuse pour le foie.</p><p class='consoude'>les feuilles peuvent être consommées cuites en soupes ou en beignets, les plus jeunes en salade. Les parties aériennes en tisane. La consoude est aussi un excellent engrais végétal (engrais vert ou purin). </p>", "<span>Coquelicot <i>(Papaver rhoeas)</i></span><p class='coquelicot'>Troubles du sommeil, agitation, maux de gorge, toux sèche</p><p class='coquelicot'>Tisane, sirop, macerats de pétales, consomation des graines. <b class='danger'>Attention</b> seul le sirop est adapté pour les enfants de moins de 6ans.</p>", "<span>Fumeterre officinale <i>(Fumaria officinalis)</i></span><p class='fumeterre'>Utile pour les sphères digestives et dermatologiques (antispasmodique, tonique, aide à la digestion). Egalement hypertensive, anti-inflamatoire, elle régule le fonctionnement de la vésicule biliaire et du foie et apaise l'eczema. Combat les nausée et soulage la constipation mème durant la grossesse.</p><p class='fumeterre'>Principalement en infusions (en sirop, teinture mère, décoctions) très amère !.</p>", "<span>Hysope <i>(Hyssopus officinalis)</i></span>", "<span>Laiteron rude et des champs <i>(Sonchus asper et arvensis)</i></span>", "<span>Lamier pourpre <i>(Lamium purpureum)</i></span>", "<span>Mauve sylvestre<i>(Malvia sylvestris)</i></span>", "<span>Millepertuis <i>(Hypericum perforatum)</i></span>", "<span>Pissenlit <i>(Taraxacum officinale)</i></span>", "<span>Plantin grand et lancéolé <i>(Plantago major et lanceolata)</i></span>", "<span>Poireaux des vignes <i>(Allium polyanthum)</i></span>", "<span>Pourpier maraîcher <i>(Portulaca oleracea)</i></span>", "<span>Roquette sauvage jaune et blanche <i>(Diplotaxis tenuifolia et erucoides)</i></span>", "<span>Ronce <i>(Rubus fruticosus)</i></span>", "<span>Souci des champs <i>(Calendula officinalis)</i></span>", "<span>Trèfle des prés <i>(Trifolium pratense)</i></span>"];
 
 
-//functions pour le contenu des fiches techniques (images à ajouter ensuite, le hotlinking n'étant pas une solution viable ici).
-function ficheAchillee (){
-	document.getElementById("name").textContent = "Achillée millefeuille (Achillea millefolium)";
-	document.getElementById("description").textContent = "antispasmodique, décongestinnante, peptique pour les troubles digestifs, cicatrisante, coagulante, anti-inflamatoire, antiseptique pour traiter les plaies même ouvertes, très utiles contre les règles très abondantes ou douloureuses."
-	document.getElementById("shape").textContent = "usage interne: en tisane ou teinture mère pour la digestion et les cycles menstruels (fleurs, feuilles), en cataplasme sur les plaies (feuilles)"
+    var liItem; // li elements.
+    var arrP = document.getElementsByTagName("p"); // p elements (to show/hide when showP2() is called).
+    
+    // create elements and add some content.
+    for (let i=0; i<plants.length; i++) {
+        liItem = document.createElement("li");
+        
+        liItem.innerHTML = plants[i];  
+    
+        var plantList = document.getElementById("plantList");
+        plantList.appendChild(liItem);
+        
+        liItem.addEventListener("click", showP2, false);
+    }
+    
+    function showP2() {
+        
+        //console.log(this); > HTMLLiElement
+        //console.log(this.children.length); > some 3 children, some 1 child
+        
+        if (this.children.length === 3) {
+            this.classList.toggle("showP");
+        }
+        
+    }
 }
-
-function ficheAil (){
-	document.getElementById("name").textContent = "Ail des ours (Allium ursinum)";
-	document.getElementById("description").textContent = "dépuratif, hypotenseur, antiseptique, rubéfiant (soulage les douleurs dues aux rhumatismes), vermifuge, très riche en vitamine C.";
-	document.getElementById("shape").textContent = "on consomme les feuilles fraiches en salade, peut ètre utilisé comme plante aromatique ou cuisiné en soupes et sauces (pesto d'ail des ours), on en fait également de liqueur.";
-}
-
-function ficheBourrache (){
-	document.getElementById("name").textContent = "Bourrache (Borago officinalis)";
-	document.getElementById("description").textContent = "sudorifique, diurétique, favorise le transit intestinal, lutte contre les dermatoses (herpes, eczéma, psoryasis) et le vieillissement cutané, aphrodisiaque.";
-	document.getElementById("shape").textContent = "usage externe : huile, interne : tisane (feuilles, fleurs séchées), fleurs fraiches pour décorer les salades. En interne il est recommandé de limiter la consomation, a fortes doses la bourrache est toxique.";
-}
-
-function ficheCamomille (){
-	document.getElementById("name").textContent = "Camomille matricaire (matricaria recutita)";
-	document.getElementById("description").textContent = "anti-inflamatoire, anti-bactérien, anti-fongique, sédatif. Traite les troubles digestifs, l'irritation occulaire ou sert d'antiprurigineux sur les crevasses, piqures d'insectes, écorchures.";
-	document.getElementById("shape").textContent = "Principalement en tisane. L'infusion refroidie peut être appliquée directement dans l'oeil, en cataplasme.";
-}
-
-
-
-
-
-/* premier essai raté ! création d'éléments à ajouter a la division fT (pour fiche technique). cette solution vaut pour une 
-   modification définitive du document (changement permanent du contenu). Dans la situation présente le contenu doit changer
-   à chaque fois que l'utilisateur clique sur le nom d'une plante. Seul l'eventListener est donc a conserver, pour la modif 
-   du contenu, je vais utiliser l'élément textContent dans une deuxième tentative ...
-
-var fT = document.getElementById("fT");
-
-var camomille = document.getElementById("camomille");
-camomille.addEventListener("click", ficheCamomille);
-
-function ficheCamomille (){
-
-	let name = document.createElement("p");
-	let node1 = document.createTextNode("Camomille matricaire (matricaria recutita)");
-	name.appendChild(node1);
-	let description = document.createElement("p");
-	let node2 = document.createTextNode("anti-inflamatoire, anti-bactérien, anti-fongique, sédatif.Traite les troubles digestifs, l'irritations occulaires ou sert d'antiprurigineux sur les crevasses, piqures d'insectes, écorchures.");
-	description.appendChild(node2);
-	let presentation = document.createElement("p");
-	let node3 = document.createTextNode("Principalement en tisane. L'infusion refroidie peut être appliquée directement dans l'oeil, en cataplasme.");
-	presentation.appendChild(node3);
-
-	fT.appendChild(name);
-	fT.appendChild(description);
-	fT.appendChild(presentation);
-
-}  */
